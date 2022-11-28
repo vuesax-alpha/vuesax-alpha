cbd7f20d319caaeb7216a89c76a97f57c3547f23
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-11-28 13:10:18 +0700

    theme: return object -> function. add vuepress app to themeDataPlugin

--- packages/documents/docs/.vuepress/theme/index.ts
+++ packages/documents/docs/.vuepress/theme/index.ts
@@ -1,6 +1,6 @@
-// import { defaultTheme } from "vuepress-vite";
-import { getDirname, path } from "@vuepress/utils";
-import type { Theme } from "@vuepress/core";
+import { resolveAppPages } from 'vuepress-vite';
+import { path } from "@vuepress/utils";
+import type { App, Theme } from "@vuepress/core";
 
 import { activeHeaderLinksPlugin } from "@vuepress/plugin-active-header-links";
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
@@ -11,57 +11,57 @@ import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 import { VuesaxAlphaThemeOptions } from "./vuesaxAlphaTheme";
 import { defaultTheme } from "vuepress-vite";
 
-// const __dirname = getDirname(import.meta.url);
-
 export const vuesaxAlphaTheme = (options?: VuesaxAlphaThemeOptions): Theme => {
-  return {
-    name: "vuepress-theme-vuesax-alpha",
-    extends: defaultTheme(options),
-    clientConfigFile: path.resolve(
-      __dirname,
-      "themeEnhance.ts"
-    ),
-    plugins: [
-      activeHeaderLinksPlugin(),
-      containerPlugin({
-        type: "tip",
-        before: (): string =>
-          `<div class="custom-container tip">${""}\n`,
-        after: (): string => "</div>\n",
-      }),
-      containerPlugin({
-        type: "warning",
-        before: (): string =>
-          `<div class="custom-container tip">${""}\n`,
-        after: (): string => "</div>\n",
-      }),
-      containerPlugin({
-        type: "danger",
-        before: (): string =>
-          `<div class="custom-container tip">${""}\n`,
-        after: (): string => "</div>\n",
-      }),
-      themeDataPlugin({
-        themeData: options
-      }),
-      registerComponentsPlugin({
-        componentsDir: path.resolve(__dirname, "global-components"),
-      }),
-      docsearchPlugin({
-        appId: "R2IYF7ETH7",
-        apiKey: "599cec31baffa4868cae4e79f180729b",
-        indexName: 'docsearch',
-        locales: {
-          '/': {
-            placeholder: 'Vuesax search',
-            translations: {
-              button: {
-                buttonText: "Vuesax search"
+  return (app: App) => {
+    return {
+      name: "vuepress-theme-vuesax-alpha",
+      // extends: defaultTheme(options),
+      clientConfigFile: path.resolve(
+        __dirname,
+        "themeEnhance.ts"
+      ),
+      plugins: [
+        activeHeaderLinksPlugin(),
+        containerPlugin({
+          type: "tip",
+          before: (): string =>
+            `<div class="custom-container tip">${""}\n`,
+          after: (): string => "</div>\n",
+        }),
+        containerPlugin({
+          type: "warning",
+          before: (): string =>
+            `<div class="custom-container tip">${""}\n`,
+          after: (): string => "</div>\n",
+        }),
+        containerPlugin({
+          type: "danger",
+          before: (): string =>
+            `<div class="custom-container tip">${""}\n`,
+          after: (): string => "</div>\n",
+        }),
+        themeDataPlugin({
+          themeData: Object.assign({}, options, app),
+        }),
+        registerComponentsPlugin({
+          componentsDir: path.resolve(__dirname, "global-components"),
+        }),
+        docsearchPlugin({
+          appId: "R2IYF7ETH7",
+          apiKey: "599cec31baffa4868cae4e79f180729b",
+          indexName: 'docsearch',
+          locales: {
+            '/': {
+              placeholder: 'Vuesax search',
+              translations: {
+                button: {
+                  buttonText: "Vuesax search"
+                }
               }
             }
           }
-        }
-      })
-    ],
-  };
+        })
+      ],
+    };
+  }
 };
