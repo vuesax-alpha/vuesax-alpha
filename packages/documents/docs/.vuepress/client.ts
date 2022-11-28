b6568eb37c05456cf59a796c2983ed8e7d024753
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-11-28 22:47:37 +0700

    remove provide siteData, use it through useSiteData

--- packages/documents/docs/.vuepress/client.ts
+++ packages/documents/docs/.vuepress/client.ts
@@ -1,17 +1,11 @@
 import { defineClientConfig } from "@vuepress/client";
-import { reactive } from "vue";
 
 import Vuesax from "vuesax-alpha";
 import "vuesax-alpha/dist/style.css";
 
-import {
-  siteDataKey
-} from "./theme/type";
-
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     app.use(Vuesax);
-    app.provide(siteDataKey, siteData);
   },
   setup() {},
   rootComponents: [],
