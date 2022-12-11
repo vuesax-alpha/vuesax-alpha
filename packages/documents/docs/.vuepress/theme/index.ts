a18c9947568e4224a8a2ce0ed5a85bd8fa00d8ae
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-12-12 00:10:13 +0700

    theme: return object theme
     return object theme instead of return function theme

--- packages/documents/docs/.vuepress/theme/index.ts
+++ packages/documents/docs/.vuepress/theme/index.ts
@@ -1,49 +1,46 @@
-import { resolveAppPages } from 'vuepress-vite';
 import { path } from "@vuepress/utils";
-import type { App, Theme } from "@vuepress/core";
+import type { Theme } from "@vuepress/core";
 
 import { activeHeaderLinksPlugin } from "@vuepress/plugin-active-header-links";
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import { themeDataPlugin } from "@vuepress/plugin-theme-data";
-import containerPlugin from "@vuepress/plugin-container";
+import {containerPlugin} from "@vuepress/plugin-container";
 
 import { VuesaxAlphaThemeOptions } from "./vuesaxAlphaTheme";
 
 export const vuesaxAlphaTheme = (options?: VuesaxAlphaThemeOptions): Theme => {
-  return (app: App) => {
-    return {
-      name: "vuepress-theme-vuesax-alpha",
-      clientConfigFile: path.resolve(
-        __dirname,
-        "themeEnhance.ts"
-      ),
-      plugins: [
-        activeHeaderLinksPlugin(),
-        containerPlugin({
-          type: "tip",
-          before: (): string =>
-            `<div class="custom-container tip">${""}\n`,
-          after: (): string => "</div>\n",
-        }),
-        containerPlugin({
-          type: "warning",
-          before: (): string =>
-            `<div class="custom-container tip">${""}\n`,
-          after: (): string => "</div>\n",
-        }),
-        containerPlugin({
-          type: "danger",
-          before: (): string =>
-            `<div class="custom-container tip">${""}\n`,
-          after: (): string => "</div>\n",
-        }),
-        themeDataPlugin({
-          themeData: Object.assign({}, options, app),
-        }),
-        registerComponentsPlugin({
-          componentsDir: path.resolve(__dirname, "global-components"),
-        }),
-      ],
-    };
-  }
+  return {
+    name: "vuepress-theme-vuesax-alpha",
+    clientConfigFile: path.resolve(
+      __dirname,
+      "client.ts"
+    ),
+    plugins: [
+      activeHeaderLinksPlugin(),
+      containerPlugin({
+        type: "tip",
+        before: (): string =>
+          `<div class="custom-container tip">${""}\n`,
+        after: (): string => "</div>\n",
+      }),
+      containerPlugin({
+        type: "warning",
+        before: (): string =>
+          `<div class="custom-container tip">${""}\n`,
+        after: (): string => "</div>\n",
+      }),
+      containerPlugin({
+        type: "danger",
+        before: (): string =>
+          `<div class="custom-container tip">${""}\n`,
+        after: (): string => "</div>\n",
+      }),
+      themeDataPlugin({
+        themeData: options,
+      }),
+      registerComponentsPlugin({
+        componentsDir: path.resolve(__dirname, "global-components"),
+      }),
+    ],
+  };
 };
