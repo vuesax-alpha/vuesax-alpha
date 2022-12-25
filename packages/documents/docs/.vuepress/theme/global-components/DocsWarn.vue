2f9808b1c3734d8b8defadbb816849bef528e06f
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-12-25 23:09:17 +0700

    theme: change docsWarn

--- packages/documents/docs/.vuepress/theme/global-components/DocsWarn.vue
+++ packages/documents/docs/.vuepress/theme/global-components/DocsWarn.vue
@@ -1,20 +1,11 @@
 <template>
   <div>
     <div class="warning custom-container">
-      <p class="custom-container-title">Documentation Vuesax-alpha</p>
+      <p class="custom-container-title">Documentation Vuesax Alpha</p>
       <p>
-        These documents refer to the latest version of
-        <strong>Vuesax-alpha</strong>, to see the documents of the previous
-        versions you can do it here 👉
-        <a target="_blank" href="https://vuesax.com/"
-          >Vuesax (4.0+)</a
-        >
+        These docs refer to the Vue 3 support, to see the documents of the previous versions you can do it here 👉
+        <a target="_blank" href="https://vuesax.com/">Vuesax (4.0+)</a>
       </p>
     </div>
   </div>
 </template>
-<script>
-export default {
-  name: "DocsWarn",
-};
-</script>
