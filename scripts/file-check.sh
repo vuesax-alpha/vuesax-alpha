8216a6ad54cf70efbacde380180f797689f0db32
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-02-17 16:47:20 +0700

    ci: rename package

--- scripts/file-check.sh
+++ scripts/file-check.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-CURRENT_PUBLISHED_TARBALL="$(npm view vuesax-plus dist.tarball)"
+CURRENT_PUBLISHED_TARBALL="$(npm view vuesax-alpha dist.tarball)"
 
 echo $CURRENT_PUBLISHED_TARBALL
 
@@ -9,4 +9,4 @@ mkdir -p tmp
 curl -o ./tmp/latest.tgz $CURRENT_PUBLISHED_TARBALL
 tar zxvf ./tmp/latest.tgz -C ./tmp
 
-diff -qr ./tmp/package ./dist/vuesax-plus | grep "Only" | cut -c 8- | sort > ./tmp/diff.txt
+diff -qr ./tmp/package ./dist/vuesax-alpha | grep "Only" | cut -c 8- | sort > ./tmp/diff.txt
