b9679153cecaa3e4e20f010eaf5898ae640abc8c
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-02-17 16:53:11 +0700

    chore: rename branch

--- internal/build-constants/src/repo.ts
+++ internal/build-constants/src/repo.ts
@@ -1,4 +1,4 @@
 export const REPO_OWNER = 'vuesax-alpha'
 export const REPO_NAME = 'vuesax-alpha'
 export const REPO_PATH = `${REPO_OWNER}/${REPO_NAME}`
-export const REPO_BRANCH = 'dev'
+export const REPO_BRANCH = 'main'
