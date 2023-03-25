0c4395172d438cf0af342f8535a19d58f95ac4d7
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2023-03-25 23:54:13 +0700

    fix(components): [input] fix model shorthand logic

--- packages/components/input/src/composables/use-input.ts
+++ packages/components/input/src/composables/use-input.ts
@@ -1,4 +1,5 @@
 import { computed, ref, shallowRef } from 'vue'
+import { UPDATE_MODEL_EVENT } from '@vuesax-alpha/constants'
 import { useInputEvent } from './use-input-event'
 import { useInputClearable } from './use-input-clearable'
 import type { InputEmitsFn, InputProps } from '../input'
@@ -6,9 +7,9 @@ import type { InputEmitsFn, InputProps } from '../input'
 export const useInput = (props: InputProps, emit: InputEmitsFn) => {
   const model = computed({
     get: () => props.modelValue,
-    set: (value) => {
+    set: (value: string | number | null | undefined) => {
       if (props.disabled || props.loading) return
-      model.value = value
+      emit(UPDATE_MODEL_EVENT, value)
     },
   })
 
