f424d60854cd687a480060e633f847a2ade33a06
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-22 00:58:28 +0700

    feat(hooks): use-floating for popper

--- packages/hooks/use-floating/index.ts
+++ packages/hooks/use-floating/index.ts
@@ -1,122 +1,175 @@
-import { isRef, onMounted, ref, unref, watchEffect } from 'vue'
-import { isClient, unrefElement } from '@vueuse/core'
-import { isNil } from 'lodash-unified'
-import { arrow as arrowCore, computePosition } from '@floating-ui/dom'
-import { buildProps, keysOf } from '@vuesax-alpha/utils'
+import { nextTick, reactive, ref } from 'vue'
+import { isClient, unrefElement, useElementBounding } from '@vueuse/core'
+import type { MaybeElementRef } from '@vueuse/core'
 
-import type { Ref, ToRefs } from 'vue'
-import type {
-  ComputePositionReturn,
-  Middleware,
-  Placement,
-  SideObject,
-  Strategy,
-  VirtualElement,
-} from '@floating-ui/dom'
+export type Placement =
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end'
 
-export const useFloatingProps = buildProps({} as const)
+export type Strategy = 'fixed' | 'absolute'
 
-export type UseFloatingProps = ToRefs<{
-  middleware: Array<Middleware>
-  placement: Placement
-  strategy: Strategy
-}>
+export type Options = {
+  /**
+   * Tooltip placement - `top` | `bottom` | `left` | `right`
+   * @default bottom
+   */
+  placement?: Placement
+  /**
+   * Tooltip strategy
+   * @default bottom
+   */
+  strategy?: Strategy
+  /**
+   * Fit popper content with reference element
+   * @default true
+   */
+  fit?: boolean
 
-type ElementRef = Parameters<typeof unrefElement>['0']
+  /**
+   * Update popper content with reference element when window is resized or scrolled
+   * @default true
+   */
+  autoUpdate?: boolean
 
-const unrefReference = (
-  elRef: ElementRef | Ref<VirtualElement | undefined>
-) => {
-  if (!isClient) return
-  if (!elRef) return elRef
-  const unrefEl = unrefElement(elRef as ElementRef)
-  if (unrefEl) return unrefEl
-  return isRef(elRef) ? unrefEl : (elRef as VirtualElement)
+  /**
+   * distance from popper to element
+   * @default 0
+   */
+  offset: number
 }
 
-export const getPositionDataWithUnit = <T extends Record<string, number>>(
-  record: T | undefined,
-  key: keyof T
-) => {
-  const value = record?.[key]
-  return isNil(value) ? '' : `${value}px`
-}
+export function useFloating(
+  reference: MaybeElementRef,
+  popper: MaybeElementRef,
+  options?: Options
+) {
+  const { placement = 'bottom', fit = true, offset = 0 } = options ?? {}
 
-export const useFloating = ({
-  middleware,
-  placement,
-  strategy,
-}: UseFloatingProps) => {
-  const referenceRef = ref<HTMLElement | VirtualElement>()
-  const contentRef = ref<HTMLElement>()
-  const x = ref<number>()
-  const y = ref<number>()
-  const middlewareData = ref<ComputePositionReturn['middlewareData']>({})
+  const popperPlacement = ref(placement)
 
-  const states = {
-    x,
-    y,
-    placement,
-    strategy,
-    middlewareData,
-  } as const
+  const destroy = () => {
+    unrefElement(popper)?.remove()
+  }
 
-  const update = async () => {
+  const updateCord = () => {
     if (!isClient) return
 
-    const referenceEl = unrefReference(referenceRef)
-    const contentEl = unrefElement(contentRef)
-    if (!referenceEl || !contentEl) return
+    nextTick(() => {
+      const triggerElement = unrefElement(reference)
+      const popperElement = unrefElement(popper)
 
-    const data = await computePosition(referenceEl, contentEl, {
-      placement: unref(placement),
-      strategy: unref(strategy),
-      middleware: unref(middleware),
-    })
+      if (!popperElement || !triggerElement) return
 
-    keysOf(states).forEach((key) => {
-      states[key].value = data[key]
+      const triggerBounding = reactive(useElementBounding(triggerElement))
+      const popperBounding = reactive(useElementBounding(popperElement))
+
+      const style = popperElement.style
+      style.position = 'fixed'
+
+      const scrollTop = window.pageYOffset
+
+      const triggerWidth = triggerBounding.width
+      const triggerHeight = triggerBounding.height
+      const triggerHalfWidth = triggerWidth / 2
+      const triggerHalfHeight = triggerHeight / 2
+
+      const popperWidth = popperBounding.width
+      const popperHaflWidth = popperWidth / 2
+      const popperHeight = popperBounding.height
+      const popperHaflHeight = popperHeight / 2
+
+      const distanceYAxis = Math.abs(triggerHalfWidth - popperHaflWidth)
+      const distanceXAxis = Math.abs(triggerHalfHeight - popperHaflHeight)
+
+      if (placement.includes('bottom') || placement.includes('top')) {
+        if (fit) style.width = `${triggerBounding.width}px`
+
+        let popperOffsetTop = triggerBounding.y + offset
+
+        if (placement.includes('bottom')) {
+          popperOffsetTop += scrollTop + triggerHeight
+        }
+
+        if (placement.includes('top')) {
+          popperOffsetTop += scrollTop - popperHeight
+        }
+
+        style.top = `${popperOffsetTop}px`
+
+        switch (placement) {
+          case 'bottom':
+          case 'top':
+            // eslint-disable-next-line no-case-declarations
+            let left = triggerBounding.x
+            if (!fit) {
+              if (triggerBounding.width > popperBounding.width) {
+                left += distanceYAxis
+              } else if (triggerBounding.width < popperBounding.width) {
+                left -= distanceYAxis
+              }
+            }
+
+            style.left = `${left}px`
+            break
+
+          case 'bottom-start':
+          case 'top-start':
+            style.left = `${triggerBounding.x}px`
+            break
+
+          case 'bottom-end':
+          case 'top-end':
+            style.left = `${triggerBounding.right - popperBounding.width}px`
+            break
+        }
+      }
+
+      if (placement.includes('left') || placement.includes('right')) {
+        if (placement.includes('left')) {
+          style.left = `${triggerBounding.x - popperBounding.width}px`
+        }
+
+        if (placement.includes('right')) {
+          style.left = `${triggerBounding.x + triggerBounding.width}px`
+        }
+
+        switch (placement) {
+          case 'left':
+          case 'right':
+            style.top = `${triggerBounding.top + distanceXAxis}px`
+            break
+
+          case 'left-start':
+          case 'right-start':
+            style.top = `${triggerBounding.top}px`
+            break
+
+          case 'left-end':
+          case 'right-end':
+            style.top = `${
+              triggerBounding.top - popperHeight + triggerHeight
+            }px`
+            break
+        }
+      }
+
+      popperPlacement.value = placement
     })
   }
 
-  onMounted(() => {
-    watchEffect(() => {
-      update()
-    })
-  })
-
   return {
-    ...states,
-    update,
-    referenceRef,
-    contentRef,
-  }
-}
-
-export type ArrowMiddlewareProps = {
-  arrowRef: Ref<HTMLElement | null | undefined>
-  padding?: number | SideObject
-}
-
-export const arrowMiddleware = ({
-  arrowRef,
-  padding,
-}: ArrowMiddlewareProps): Middleware => {
-  return {
-    name: 'arrow',
-    options: {
-      element: arrowRef,
-      padding,
-    },
-
-    fn(args) {
-      const arrowEl = unref(arrowRef)
-      if (!arrowEl) return {}
-
-      return arrowCore({
-        element: arrowEl,
-        padding,
-      }).fn(args)
-    },
+    update: updateCord,
+    destroy,
+    placement: popperPlacement,
   }
 }
