1e0a7997c347095e81aa7b0ef94abd3217a26254
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-22 00:59:24 +0700

    feat(constants): popper constants

---
+++ packages/constants/popper.ts
@@ -0,0 +1,14 @@
+export const placements = [
+  'top',
+  'top-start',
+  'top-end',
+  'bottom',
+  'bottom-start',
+  'bottom-end',
+  'left',
+  'left-start',
+  'left-end',
+  'right',
+  'right-start',
+  'right-end',
+] as const
