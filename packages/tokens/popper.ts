40aab2db9beb647a038e7e6e693cd0974c2b7bb5
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-23 22:25:18 +0700

    fix(tokens): [popper] change popper context

--- packages/tokens/popper.ts
+++ packages/tokens/popper.ts
@@ -1,3 +1,5 @@
+import type { PopperTriggerType } from '@vuesax-alpha/components'
+import type { Arrayable } from '@vuesax-alpha/utils'
 import type { InjectionKey, Ref } from 'vue'
 
 export type Measurable = {
@@ -13,11 +15,19 @@ export type PopperContext = {
   triggerRef: Ref<HTMLElement | undefined>
   contentRef: Ref<HTMLElement | undefined>
   referenceRef: Ref<Measurable | undefined>
+
+  controlled: Ref<boolean>
+  id: Ref<string>
   open: Ref<boolean>
-  startShow: () => void
-  stopShow: () => void
-  startHide: () => void
-  stopHide: () => void
+  trigger: Ref<Arrayable<PopperTriggerType>>
+  onOpen: (e?: Event) => void
+  onClose: (e?: Event) => void
+  onToggle: (e: Event) => void
+  onShow: () => void
+  onHide: () => void
+  onBeforeShow: () => void
+  onBeforeHide: () => void
+  updatePopper: (shouldUpdateZIndex?: boolean) => void
 }
 
 export type PopperContentInjectionContext = {
