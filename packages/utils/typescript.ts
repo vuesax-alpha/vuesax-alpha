312365183275f7898b433987a3bdbcb5d803b1d2
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-27 22:30:09 +0700

    feat(utils): export MaybeRef

--- packages/utils/typescript.ts
+++ packages/utils/typescript.ts
@@ -1,3 +1,5 @@
+import type { Ref } from 'vue'
+
 export const mutable = <T extends readonly any[] | Record<string, unknown>>(
   val: T
 ) => val as Mutable<typeof val>
@@ -13,3 +15,4 @@ export type Nullable<T> = T | null
 
 export type Arrayable<T> = T | T[]
 export type Awaitable<T> = Promise<T> | T
+export type MaybeRef<T> = T | Ref<T>
