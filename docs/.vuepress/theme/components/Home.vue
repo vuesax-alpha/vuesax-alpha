1826cc223b156f1973e053b033b625467678abc0
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-05-28 12:20:58 +0700

    chore: lint+format

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -94,7 +94,6 @@ import { computed, onMounted, reactive, ref } from 'vue'
 import { usePageFrontmatter } from '@vuepress/client'
 // @ts-ignore
 import { useThemeData } from '@vuepress/plugin-theme-data/client'
-import { VsThemeHomeFeatureOption } from '../shared/frontmatter/home'
 import NavLink from './NavLink.vue'
 import Footer from './Footer.vue'
 import Illustration1 from './HomeIllustration1.vue'
@@ -104,8 +103,11 @@ import Illustration4 from './HomeIllustration4.vue'
 import HomeComponent from './HomeComponent.vue'
 import HomeTwitter from './HomeTwitter.vue'
 import HomeUses from './HomeUses.vue'
+import type {
+  VsThemeHomeFeatureOption,
+  VsThemeProjectHomePageFrontmatter,
+} from '../shared/frontmatter/home'
 
-import type { VsThemeProjectHomePageFrontmatter } from '../shared/frontmatter/home'
 import type { VuesaxAlphaThemeOptions } from '~/vuesaxAlphaTheme'
 
 export type PageButtons = {
