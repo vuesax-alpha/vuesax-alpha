d3b6c110a27a01e693b5462340ad51f82d22b870
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-06-08 12:47:35 +0700

    fix(docs): [pagination] display correctly on small device

--- docs/.vuepress/components/pagination/not-margin.vue
+++ docs/.vuepress/components/pagination/not-margin.vue
@@ -16,4 +16,11 @@ const page = ref(1)
     margin: 10px 0px;
   }
 }
+
+@media screen and (max-width: 768px) {
+  .con-pagination {
+    overflow: scroll;
+    justify-content: flex-start;
+  }
+}
 </style>
