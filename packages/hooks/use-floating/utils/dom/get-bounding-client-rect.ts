484c3141a64a0574554fec67c0d363a0efbaf86a
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-06-13 11:24:57 +0700

    fix(hooks): [use-floating] default properties when element isNil

--- packages/hooks/use-floating/utils/dom/get-bounding-client-rect.ts
+++ packages/hooks/use-floating/utils/dom/get-bounding-client-rect.ts
@@ -1,8 +1,12 @@
 import { rectToClientRect } from '../rect-to-client-rect'
 import type { ClientRectObject } from '../../types'
 
-export function getBoundingClientRect(element: Element): ClientRectObject {
-  const clientRect = element?.getBoundingClientRect()
+export function getBoundingClientRect(element?: Element): ClientRectObject {
+  if (!element) {
+    return rectToClientRect({ width: 0, height: 0, x: 0, y: 0 })
+  }
+
+  const clientRect = element.getBoundingClientRect()
 
   const x = clientRect.left
   const y = clientRect.top
