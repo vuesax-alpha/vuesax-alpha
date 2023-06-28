3134ec7032c7007feb8d3ef7d58330f9ce5a7e92
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-06-28 07:45:24 +0700

    refactor(hooks): [use-prop] unref value when prop passed is ref object

--- packages/hooks/use-prop/index.ts
+++ packages/hooks/use-prop/index.ts
@@ -1,7 +1,7 @@
-import { computed, getCurrentInstance } from 'vue'
+import { computed, getCurrentInstance, unref } from 'vue'
 import type { ComputedRef } from 'vue'
 
 export const useProp = <T>(name: string): ComputedRef<T | undefined> => {
   const vm = getCurrentInstance()!
-  return computed(() => (vm.proxy?.$props as any)[name] ?? undefined)
+  return computed(() => unref((vm.proxy?.$props as any)[name]) ?? undefined)
 }
