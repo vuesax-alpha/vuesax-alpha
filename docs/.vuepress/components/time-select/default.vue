9770bf913583f35f03ec7e1e35c3a38190511aac
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-08-02 07:35:37 +0700

    fix(docs): [time-select]

---
+++ docs/.vuepress/components/time-select/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="center">
+    <vs-time-select
+      v-model="value"
+      start="08:30"
+      step="00:15"
+      end="18:30"
+      placeholder="Select time"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value = ref('')
+</script>
